d1fa82543ddecc02a33a708ac732e0d0100cf5c2
Amer ALRAKIK <alrakikamer@gmail.com>
2024-12-03 13:07:29 +0100

    added oneint keyword, and fix small problems

--- DALTON/include/turin.h
+++
@@ -1,6 +0,0 @@
-!      LOGICAL TOURA
-!      CHARACTER*8 space , ANGO
-!      REAL*8     LX, LY, LZ
-!      REAL*8     ax, ay, az
-
-!      COMMON TOURA , space , ango , Lx , LY , Lz , ax , ay , az 
